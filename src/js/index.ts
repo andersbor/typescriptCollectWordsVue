901151fac3e3bedc742fb34e602571ab04161d59
Anders Børjesson <anbo@zealand.dk>
2020-08-26 11:03:53 +0200

    done

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,23 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
-
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+new Vue({
+    el: "#app",
+    data: {
+        word: "",
+        words: [],
+        message: ""
+    },
+    methods: {
+        save(word: string) {
+            this.words.push(word)
+        },
+        show() {
+            if (this.words.length > 0)
+                this.message = this.words.toString()
+            else
+                this.message = "No words"
+        },
+        clear() {
+            this.words = []
+            this.message = ""
+        }
+    }
+}) 
